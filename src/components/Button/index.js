0c199af7033261e071eb387cbbae832e8c11b9b5
QuangDoan <dv.quang.2149@aptechlearning.edu.vn>
2023-08-03 23:53:35 +0700

    setting moi truong

---
+++ src/components/Button/index.js
@@ -0,0 +1,9 @@
+import myAlert from "~/myAlert";
+function Button(){
+    return(
+        <div>
+            <button onClick={myAlert}>Click me!</button>
+        </div>
+    )
+}
+export default Button;
---
+++ src/myAlert.js
@@ -0,0 +1,5 @@
+
+const myAlert = () => {
+    alert("say hi");
+}
+export default myAlert;
