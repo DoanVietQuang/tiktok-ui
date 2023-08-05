d84100f5637b96558abb6f2545bdd89b69aba27d
QuangDoan <dv.quang.2149@aptechlearning.edu.vn>
2023-08-05 22:31:39 +0700

    Dựng khung Layout mặc định

--- src/components/Layout/DefaultLayout/Sidebar/index.js
+++ src/components/Layout/DefaultLayout/Sidebar/index.js
@@ -1,5 +1,13 @@
+import classNames from 'classnames/bind';
+import style from './Sidebar.module.scss';
+const cx = classNames.bind(style);
+
 function Sidebar() {
-    return <h2>Sidebar</h2>;
+    return (
+        <aside className={cx('wrapper')}>
+            <h2>Sidebar</h2>
+        </aside>
+    );
 }
 
 export default Sidebar;
--- src/components/Layout/DefaultLayout/index.js
+++ src/components/Layout/DefaultLayout/index.js
@@ -1,13 +1,15 @@
 import Header from '~/components/Layout/components/Header';
 import Sidebar from './Sidebar';
-
+import classNames from 'classnames/bind';
+import styles from './DefautLayout.module.scss';
+const cx = classNames.bind(styles);
 function DefaultLayout({ children }) {
     return (
-        <div>
+        <div className={cx('wrapper')}>
             <Header />
-            <div className="container">
+            <div className={cx('container')}>
                 <Sidebar />
-                <div className="content">{children}</div>
+                <div className={cx('content')}>{children}</div>
             </div>
         </div>
     );
