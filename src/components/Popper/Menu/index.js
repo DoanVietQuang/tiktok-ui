88c6f31407846748a6f15a4b16555cfe8290efbe
QuangDoan <dv.quang.2149@aptechlearning.edu.vn>
2023-09-02 21:59:45 +0700

    css menu-item

---
+++ src/components/Popper/Menu/index.js
@@ -0,0 +1,27 @@
+import classNames from 'classnames/bind';
+import Tippy from '@tippyjs/react/headless';
+import styles from './Menu.module.scss';
+import MenuItem from './MenuItem';
+import { Wrapper as PopperWrapper } from '~/components/Popper';
+const cx = classNames.bind(styles);
+function Menu({ children, items = [] }) {
+    const renderItems = () => {
+        return items.map((item, index) => <MenuItem key={index} data={item} />);
+    };
+    return (
+        <Tippy
+            delay={[0,700]}
+            interactive
+            placement="bottom-end"
+            render={(attrs) => (
+                <div className={cx('menu-list')} tabIndex="-1" {...attrs}>
+                    <PopperWrapper className={cx('menu-poper')}>{renderItems()}</PopperWrapper>
+                </div>
+            )}
+        >
+            {children}
+        </Tippy>
+    );
+}
+
+export default Menu;
